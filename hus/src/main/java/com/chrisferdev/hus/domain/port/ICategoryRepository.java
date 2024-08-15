a7197dce9df8a0e2bbb46c97bc2b5db7f6e9a434
ChrisferDev <ferchris82@hotmail.com>
2024-08-15 18:19:41 -0500

    V0.1

--- hus/src/main/java/com/chrisferdev/hus/domain/port/ICategoryRepository.java
+++ hus/src/main/java/com/chrisferdev/hus/domain/port/ICategoryRepository.java
@@ -1,8 +1,6 @@
 package com.chrisferdev.hus.domain.port;
 
 
-//Definir todos los métodos que nos van a permitir trabajar con la entidad categoría
-
 import com.chrisferdev.hus.domain.model.Category;
 
 public interface ICategoryRepository {
