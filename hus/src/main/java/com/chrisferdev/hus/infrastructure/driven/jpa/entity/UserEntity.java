613dbff956e0e8d41c874e4c80289fc54f1655d3
ChrisferDev <ferchris82@hotmail.com>
2024-09-04 18:09:04 -0500

    Versión 0.2

--- hus/src/main/java/com/chrisferdev/hus/infrastructure/driven/jpa/entity/UserEntity.java
+++ hus/src/main/java/com/chrisferdev/hus/infrastructure/driven/jpa/entity/UserEntity.java
@@ -1,4 +1,26 @@
 package com.chrisferdev.hus.infrastructure.driven.jpa.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String documentId;
+    private String phoneNumber;
+    private LocalDate birthDate;
+    private String email;
+    private String password;
 }
