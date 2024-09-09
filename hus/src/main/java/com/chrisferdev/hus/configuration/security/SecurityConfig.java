ce8b6d33648a6f55ac95bf3ccf641083adc1bd88
ChrisferDev <ferchris82@hotmail.com>
2024-09-09 13:06:33 -0500

    Versión Security

--- hus/src/main/java/com/chrisferdev/hus/configuration/security/SecurityConfig.java
+++ hus/src/main/java/com/chrisferdev/hus/configuration/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.chrisferdev.hus.configuration.security;
 import com.chrisferdev.hus.configuration.security.jwt.JWTAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private static final String ROLE_ADMIN = "ADMIN";
+    private static final String ROLE_WAREHOUSE_ASSISTANT = "WAREHOUSE_ASSISTANT";
 
     private JWTAuthorizationFilter jwtAuthorizationFilter;
 
@@ -26,10 +28,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf( csrf-> csrf.disable()).authorizeHttpRequests(
-                aut -> aut.requestMatchers("/api/admin/categories/**").hasRole(ROLE_ADMIN)
-                        .requestMatchers("/api/admin/brands/**").hasRole(ROLE_ADMIN)
-                        .requestMatchers("/api/admin/products/**").hasRole(ROLE_ADMIN)
-                        .requestMatchers("/api/security/register/**").permitAll()
+                aut -> aut.requestMatchers(HttpMethod.POST, "/api/categories/**").hasRole(ROLE_ADMIN)
+                        .requestMatchers("/api/brands/**").hasRole(ROLE_ADMIN)
+                        .requestMatchers("/api/security/register/**").hasRole(ROLE_ADMIN)
+                        .requestMatchers(HttpMethod.POST,"/api/products/**").hasRole(ROLE_WAREHOUSE_ASSISTANT)
+                        .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
                         .requestMatchers( "/api/security/login**").permitAll().anyRequest().authenticated()
 
         ).addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
--- hus/src/main/java/com/chrisferdev/hus/domain/api/usecase/RegisterUserServiceImpl.java
+++ hus/src/main/java/com/chrisferdev/hus/domain/api/usecase/RegisterUserServiceImpl.java
@@ -29,7 +29,7 @@ public class RegisterUserServiceImpl {
     public UserRequest saveUser(UserRequest userRequest) {
         validateUserRequest(userRequest);
         if (userRequest.getUserType() == null) {
-            userRequest.setUserType(UserType.ADMIN);
+            userRequest.setUserType(UserType.WAREHOUSE_ASSISTANT);
         }
 
         userRequest.setPassword(passwordEncoder.encode(userRequest.getPassword()));
